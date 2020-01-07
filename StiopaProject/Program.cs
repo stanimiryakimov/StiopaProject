40a8a554ffa43375fbae61c36e928cf93ecf1eb8
Stan Yakimov 🦁 <stanyakimov@Stans-MacBook-Air.local>
2020-01-07 23:36:28 +0200

    Initial VS commit

---
+++ StiopaProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StiopaProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
